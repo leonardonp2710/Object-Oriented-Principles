fc9eda108385244d3da903fcfb8330b23e36d00a
leonardonp2710 <84817106+leonardonp2710@users.noreply.github.com>
2023-10-16 22:06:49 +0100

    TestPerson file updated

--- Lab1/src/TestPerson.java
+++ Lab1/src/TestPerson.java
@@ -3,8 +3,8 @@ public class TestPerson {
 	public static void main(String[] args) {
 		Person person = new Person(5, "083 344 2838", "Jack");
 		
-		System.out.println(person);
-		
+		System.out.println(person.setName("Leo"));
+		System.out.println(person.getName());
 	
 		person.setJumpCount(5);
 	
